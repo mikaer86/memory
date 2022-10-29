bf0cc10ad9a8c2845bf689962170df6650d54223
mikaer86 <mikaeleriksson123@gmail.com>
2022-10-30 00:44:23 +0200

    displayValue function

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-let card1 = document.querySelector("#card1");
-let card2 = document.querySelector("#card2");
-let card3 = document.querySelector("#card3");
-let card4 = document.querySelector("#card4");
-let cards = [card1, card2, card3, card4];
-let pair = [];
-
 function reloadGame() {
   const resetButton = document.querySelector("#reset-btn");
 
@@ -14,51 +7,34 @@ function reloadGame() {
   resetButton.addEventListener("click", reset);
 }
 
-function createCard() {
-  let cardAmount = 6;
-  let createDiv = document.createElement("div");
-  let createCardInner = (createDiv.className = "card__inner");
-  let createFaceFront = (createCardInner.className =
-    "card__face card__face--front");
-  let createFaceBack = (createFaceFront.className =
-    "card__face card__face--back");
+function displayValue() {
+  window.addEventListener("load", function () {
+    let targetCardValues = document.querySelectorAll(".card__face--back");
+
+    let cardNumbers = [1, 1, 2, 2];
+
+    for (let targetCardValue of targetCardValues) {
+      let h2 = document.createElement("h2");
+      targetCardValue.append(h2);
+      for (let i = cardNumbers.length; i < 5; i++) {
+        let randomNumber = Math.floor(Math.random() * i);
+        h2.append(randomNumber);
+      }
+    }
+  });
 }
-function shuffle() {
-  let cardGenerator = createCard();
-  let randomCard = Math.floor(Math.random() * cards.length);
 
-  for (let i = 0; i <= randomCard.length; i++) {
-    return i.append();
-  }
-}
+function checkCard() {
+  let cards = document.querySelectorAll(".card__inner");
 
-function clickCard() {
-  {
-    card1.addEventListener("click", function (e) {
-      card1.classList.toggle("is-flipped");
-
-      e.stopPropagation();
-    });
-
-    card2.addEventListener("click", function (e) {
-      card2.classList.toggle("is-flipped");
-
-      e.stopPropagation();
-    });
-
-    card3.addEventListener("click", function (e) {
-      card3.classList.toggle("is-flipped");
-
-      e.stopPropagation();
-    });
-
-    card4.addEventListener("click", function (e) {
-      card4.classList.toggle("is-flipped");
-
-      e.stopPropagation();
+  for (let card of cards) {
+    card.addEventListener("click", function () {
+      card.classList.toggle("is-flipped");
     });
   }
 }
 
-shuffle();
-clickCard();
+function pairCards() {}
+
+displayValue();
+checkCard();
